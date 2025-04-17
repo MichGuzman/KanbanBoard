f3a45122e4f7f06f572dc7d444a0227f68fbcab6
MichelleGuzman <michguzman@live.com>
2025-04-17 11:24:51 -0600

    Actualización de login funcionando correctamente

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -6,7 +6,6 @@ import bcrypt from 'bcrypt';
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
   const { username , password } = req.body
-
   const user = await User.findOne({
     where: { username },
   });
@@ -21,16 +20,11 @@ export const login = async (req: Request, res: Response) => {
   }
 
   const secretKey = process.env.JWT_SECRET_KEY || '';
-
   const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-
   return res.json({ token });
-
 };
 
 const router = Router();
-
 // POST /login - Login a user
 router.post('/login', login);
-
 export default router;
